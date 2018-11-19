b92ba81f8a27d666b21fb834f6bde0ad092ec01f
Stanley Chan <sny1005@users.noreply.github.com>
2018-11-20 00:22:58 +0800

    studied creating new Promise for ordinary callback functions

--- pptr.js
+++ pptr.js
@@ -1,6 +1,7 @@
 const puppeteer = require('puppeteer-core');
 const fs = require('fs');
 const cheerio = require('cheerio');
+// const Promise = require('promise');
 
 url = 'https://manhua.dmzj.com/yongzhecizhibuganle/76257.shtml';
 
@@ -10,25 +11,33 @@ async function main(){
 	const page = await browser.newPage();
 	console.log("New tab created");
 
-	const resp = await page.goto(url);
-	console.log("Page loaded");
-	console.log(resp.status(), resp.statusText());
+	try{
+		option = {
+			"timeout":0
+		}
+		const resp = await page.goto(url, option);
+		console.log("Page loaded");
+		if(resp.ok()){
+
+			console.log("Evaluating comic pages...");
+			var comicName = page.evaluate(() => g_comic_name);
+			var comicChapter = page.evaluate(() => g_chapter_name);
+			var comicPages = page.evaluate(() => pages);
+
+			// comicDtl = await Promise.all([comicName, comicChapter, comicPages]);
+			comicDtl = await page.evaluate(() => {return {'cname':g_comic_name, 'cchapter':g_chapter_name, 'clinks':pages}});
+
+			DownloadComic(comicDtl);
+			
+			// console.log(comicPages);
+		} else {
+			console.error(resp.status(), resp.statusText());
+		}
+	} catch(err) {
+		console.error(err);
+	}
 	
-	// page.once('domcontentloaded')
-/* 	resp.then(() =>{
-		if resp.
-	}) */
-
-/* 		.then(() => {return page.content()})
-		.catch((err) => {console.log(err)}); */
-	
-
-
-
-
-
-
-/* 	fs.writeFile('./output.html', content, (err) => {
+	/* 	fs.writeFile('./output.html', content, (err) => {
 	if(err){
 	    console.log('Error occured during saving output to file');
 	}
@@ -43,7 +52,34 @@ async function main(){
 	console.log("Browser closed");
 };
 
-function parseImageSrcUrl(document){
+async function DownloadComic(comicDetails){
+	const imgSrcDomain = 'https://images.dmzj.com/';
+	const comicName = comicDetails['cname'];
+	const comicChapter = comicDetails['cchapter'];
+	const comicPages = comicDetails['clinks'];
+
+	// console.log(comicName, comicChapter, comicPages);
+	const urlFD = await OpenFile(comicName+comicChapter+'.txt');
+	
+	// await fs.open('./'+comicName+comicChapter+'.txt', 'w', async (err, fd) => {
+
+	console.log(typeof(urlFD));
+	comicPages.forEach(element => {
+		
+	});
+}
+
+function OpenFile(fileName){
+	// https://nodejs.org/api/fs.html#fs_fs_createwritestream_path_options
+	return new Promise((resolve, reject) => {
+		fs.open('./'+fileName, 'w', (err,fd) => {
+			if (err) return reject(err);
+			else return resolve(fd);
+		});
+	});
+}
+
+function ParseImageSrcUrl(document){
 	const $ = document;
 
 }
